ed53ead910de3a204cc514f5ba281a69544836be
Hellseher <sharlatanus@gmail.com>
2015-08-26 22:47:03 +0100

    [E] ./src/usage.sh
        Add more option for usage template
        take and idea from LFS201 cource

--- src/usage.sh
+++ src/usage.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # File          :  usage.sh
 # Created       :  Wed 26 Aug 2015 22:42:43
-# Last Modified :  Wed 26 Aug 2015 22:43:13
+# Last Modified :  Wed 26 Aug 2015 22:46:26
 # Maintainer    :  sharlatan, <sharlatanus@gmail.com>
 # License       :  Same as Bash (GPL)
 # Credits:
@@ -26,3 +26,37 @@ usage() {
     echo "Example: `basename $0` --install LFD411"
     exit 0
 }
+
+
+# Command option parsing
+OPTS="$*"
+while [[ $# -gt 0 ]] ; do
+    case "$1" in
+        --debug) DEBUG=y ;;
+        --distro) LIST_DISTRO=y ;;
+        --detect-vm) DETECT_VM=y ;;
+        -i|--install) INSTALL=y ;;
+        --force-update) FORCEUPDATE=y ;;
+        --gather-info) gather_info ;;
+        --json) JSON=y ;;
+        -l|--list) LIST_COURSES=y; break ;;
+        -L|--list-requirements) LIST_REQS=y ;;
+        -C|--no-course-materials) NOCM=y ;;
+        -I|--no-install) NOINSTALL=y ;;
+        -R|--no-recommends) NORECOMMENDS=y ;;
+        -D|--override-distro) DISTRO=$2; shift;;
+        --progress) PROGRESS=y ;;
+        --simulate-failure) SIMULATE_FAILURE=y ;;
+        --try-all-courses) TRY_ALL_COURSES=y ;;
+        --update-beta) UPDATE=beta; VERBOSE=y ;;
+        --update) UPDATE=y; VERBOSE=y ;;
+        --verify) VERIFY=y ;;
+        -v|--verbose) VERBOSE=y ;;
+        -V|--version) echo $VERSION; exit 0;;
+        LFD*|LFS*) COURSE="$COURSE $1";;
+        [0-9][0-9][0-9]) COURSE="$COURSE LFD$1";;
+        -h*|--help*|*) usage ;;
+    esac
+    shift
+done
+CMD=$0
