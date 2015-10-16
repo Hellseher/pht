117424e9593b571f2dbd69fc3accbc9900173359
Hellseher <sharlatanus@gmail.com>
2015-10-16 17:04:39 +0100

    [E] solve abs path problem...     I thought it could be easer, but nothing happens that way.     Played around with linking ./bin/* to ~/.local/bin and exporting to $PATH
        modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #! /bin/sh
 # File          :  install.sh
 # Created       :  Fri 16 Oct 2015 14:50:01
-# Last Modified :  Fri 16 Oct 2015 14:56:24
+# Last Modified :  Fri 16 Oct 2015 17:03:39
 # Maintainer    :  sharlatan, <sharlatanus@gmail.com>
 # License       :  Same as Bash (GPL)
-#
+# Credits       : http://www.cyberciti.biz/faq/bash-loop-over-file/
 #..:: Description ::..
+# depends on realpath... try to replace for POSIX disiion
 #
 # 
 LOCAL_BIN=$HOME/.local/bin
+ABS_PATH_BIN=$(realpath $0 |  grep -Po "(?:(\S)*)(tiny-toolbox)")/bin/*
 
 if [[ ! -d ${LOCAL_BIN} ]]; then
+    # recurcivle create ~/.local/bin path
     mkdir -p ${LOCAL_BIN}
-    ln -s $(ls ./bin) ${LOCAL_BIN}
+
+    for bin in ${ABS_PATH_BIN}; do
+        ln -s $bin ${LOCAL_BIN}
+        #statements
+    done
 fi
 
