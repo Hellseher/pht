a9260a79e24d499f287f2bc1846a6c110411bda3
Hellseher <sharlatanus@gmail.com>
2015-10-24 13:45:32 +0100

    [E] copsor; install; try to link new var list;     Struggling with loading additional variables...     https://stackoverflow.com/questions/9772036/pass-all-variables-from-one-shellscript-to-another
            modified:   bin/copsor
            modified:   bin/extract
            modified:   bin/gitup
            modified:   bin/picsort
            modified:   install.sh
    
            new file:   bin/load-libs
            new file:   lib/vars-lib

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #! /bin/sh
 # File          :  install.sh
 # Created       :  Fri 16 Oct 2015 14:50:01
-# Last Modified :  Fri 16 Oct 2015 17:03:39
+# Last Modified :  Sat 24 Oct 2015 13:27:17
 # Maintainer    :  sharlatan, <sharlatanus@gmail.com>
 # License       :  Same as Bash (GPL)
 # Credits       : http://www.cyberciti.biz/faq/bash-loop-over-file/
+#               : https://regex101.com/
+#
 #..:: Description ::..
 # depends on realpath... try to replace for POSIX disiion
 #
 # 
 LOCAL_BIN=$HOME/.local/bin
-ABS_PATH_BIN=$(realpath $0 |  grep -Po "(?:(\S)*)(tiny-toolbox)")/bin/*
+ABS_PATH_BIN=$(realpath $0 | grep -Po "(?:(\S)*)(tiny-toolbox)")/bin/*
 
 if [[ ! -d ${LOCAL_BIN} ]]; then
-    # recurcivle create ~/.local/bin path
+    # recursive create ~/.local/bin path
     mkdir -p ${LOCAL_BIN}
 
-    for bin in ${ABS_PATH_BIN}; do
-        ln -s $bin ${LOCAL_BIN}
-        #statements
-    done
 fi
+# force create symlink to ./bin/*
+for bin in ${ABS_PATH_BIN}; do
+    ln -sf $bin ${LOCAL_BIN}
+done
 
